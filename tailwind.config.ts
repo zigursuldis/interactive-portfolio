946f7794bfdac90eed40c13c8592c5e50875b562
Uldis Zigurs <uldis.zigurs@outlook.com>
2024-10-12 22:04:25 +0300

    set up colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,21 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "bg-primary": "#282725",
+        "bg-secondary": "#2C2C2E",
+        "bg-tertiary": "#1F1F1F",
+        "bg-quaternary": "#141414",
+        "text-primary": "#F7F5F1",
+        accent: {
+          light: "#FF9A76",
+          DEFAULT: "#FF6F3C",
+          dark: "#D35400",
+        },
+        neutral: {
+          light: "#ECE4DB",
+          DEFAULT: "#8E8E93",
+          dark: "#2C2C2E",
+        },
       },
     },
   },
