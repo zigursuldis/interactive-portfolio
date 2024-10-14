111132f69aef30182c07af5d63067ac903c5adde
Uldis Zigurs <uldis.zigurs@outlook.com>
2024-10-15 00:25:59 +0300

    index fix, center page on large sceeens

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,15 @@ import Timeline from "@/components/timeline/timeline";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen mx-8 lg:mx-12 max-w-[1200px] items-center">
-      <Header />
-      <main className="grow w-full">
-        <Banner />
-        <Timeline />
-      </main>
-      <Footer />
+    <div className="flex w-full justify-center">
+      <div className="flex flex-col min-h-screen mx-8 lg:mx-12 max-w-[1200px] w-full items-center">
+        <Header />
+        <main className="grow w-full">
+          <Banner />
+          <Timeline />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
