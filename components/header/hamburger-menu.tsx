edaa1d682a8e038ee5a3a2f50e2d5534f2ea9148
Uldis Zigurs <uldis.zigurs@outlook.com>
2024-11-06 23:12:59 +0200

    fix deployment error

--- components/header/hamburger-menu.tsx
+++ components/header/hamburger-menu.tsx
@@ -24,51 +24,6 @@ export default function HamburgerMenu() {
     }
   };
 
-  const portal = createPortal(
-    <div
-      className="fixed top-0 left-0 w-screen h-screen backdrop-blur-[1px] z-50"
-      onClick={onClickOverlay}
-    >
-      <div
-        className="absolute right-0 bg-bg-secondary w-10/12 h-screen animate-slideInFromRight"
-        ref={modalRef}
-        tabIndex={0}
-      >
-        <div className="flex justify-between items-center">
-          <h1 className="text-md md:text-base text-accent duration-200 text-nowrap font-departure-mono [text-shadow:_0_4px_0_#000] ml-4">
-            Uldis Zigurs
-          </h1>
-          <button className="p-1" onClick={() => setIsOpen(false)}>
-            <CloseIcon
-              className="hover:fill-accent-dark duration-200 fill-accent"
-              width={32}
-              height={32}
-            />
-          </button>
-        </div>
-        <Divider />
-        <nav
-          className="flex flex-col justify-center items-center font-departure-mono mt-2"
-          onClick={onClickNavigationContainer}
-        >
-          <a className="p-1 hover:text-accent-light" href="#about_me">
-            About me
-          </a>
-          <a className="p-1 hover:text-accent-light" href="#skills">
-            Skills
-          </a>
-          <a className="p-1 hover:text-accent-light" href="#work">
-            Work
-          </a>
-          <a className="p-1 hover:text-accent-light" href="#contact_me">
-            Contact
-          </a>
-        </nav>
-      </div>
-    </div>,
-    document.body
-  );
-
   useEffect(() => {
     if (isOpen) {
       document.body.style.overflow = "hidden";
@@ -87,6 +42,10 @@ export default function HamburgerMenu() {
     }
   }, [isOpen]);
 
+  if (!document) {
+    return null;
+  }
+
   return (
     <div className="p-1 ml-1 md:hidden h-8">
       {isOpen ? (
@@ -99,7 +58,52 @@ export default function HamburgerMenu() {
         </button>
       )}
 
-      {isOpen ? portal : null}
+      {isOpen
+        ? createPortal(
+            <div
+              className="fixed top-0 left-0 w-screen h-screen backdrop-blur-[1px] z-50"
+              onClick={onClickOverlay}
+            >
+              <div
+                className="absolute right-0 bg-bg-secondary w-10/12 h-screen animate-slideInFromRight"
+                ref={modalRef}
+                tabIndex={0}
+              >
+                <div className="flex justify-between items-center">
+                  <h1 className="text-md md:text-base text-accent duration-200 text-nowrap font-departure-mono [text-shadow:_0_4px_0_#000] ml-4">
+                    Uldis Zigurs
+                  </h1>
+                  <button className="p-1" onClick={() => setIsOpen(false)}>
+                    <CloseIcon
+                      className="hover:fill-accent-dark duration-200 fill-accent"
+                      width={32}
+                      height={32}
+                    />
+                  </button>
+                </div>
+                <Divider />
+                <nav
+                  className="flex flex-col justify-center items-center font-departure-mono mt-2"
+                  onClick={onClickNavigationContainer}
+                >
+                  <a className="p-1 hover:text-accent-light" href="#about_me">
+                    About me
+                  </a>
+                  <a className="p-1 hover:text-accent-light" href="#skills">
+                    Skills
+                  </a>
+                  <a className="p-1 hover:text-accent-light" href="#work">
+                    Work
+                  </a>
+                  <a className="p-1 hover:text-accent-light" href="#contact_me">
+                    Contact
+                  </a>
+                </nav>
+              </div>
+            </div>,
+            document.body
+          )
+        : null}
     </div>
   );
 }
