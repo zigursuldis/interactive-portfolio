f069688d7a6064a8732c430418e511715fbb0eeb
Uldis Zigurs <uldis.zigurs@outlook.com>
2024-11-06 23:41:49 +0200

    revert header explictly force render client side

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
-import dynamic from "next/dynamic";
 import Banner from "@/components/banner";
 import Footer from "@/components/footer";
+import Header from "@/components/header/header";
 import Timeline from "@/components/timeline/timeline";
 import Skills from "@/components/skills/skills";
 import ContactMe from "@/components/contact-me";
 import Divider from "@/components/divider";
 import Work from "@/components/work/work";
-const Header = dynamic(() => import("@/components/header/header"), {
-  ssr: false,
-});
 
 export default function Home() {
   return (
@@ -22,9 +19,7 @@ export default function Home() {
           </h2>
           <Divider />
           <Timeline />
-          <h2 className="text-xl font-departure-mono" id="skills">
-            Skills & technologies
-          </h2>
+          <h2 className="text-xl font-departure-mono">Skills & technologies</h2>
           <Divider />
           <Skills />
           <h2 className="text-xl font-departure-mono" id="work">
@@ -32,9 +27,7 @@ export default function Home() {
           </h2>
           <Divider />
           <Work />
-          <h2 className="text-xl font-departure-mono" id="contact_me">
-            Contact me
-          </h2>
+          <h2 className="text-xl font-departure-mono">Contact me</h2>
           <Divider />
           <ContactMe />
         </main>
